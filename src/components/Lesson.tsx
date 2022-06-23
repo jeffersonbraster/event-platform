d25096dbd74d322dc31461da8e3a6827126b2a72
Jefferson <jeferson.oliveira@g.globo>
2022-06-23 00:08:16 -0300

    criando rotas para as aulas, estilizando componente de videos

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -11,14 +12,14 @@ interface LessonProps {
 
 const Lesson = ({ title, slug, availableAt, type }: LessonProps) => {
   return (
-    <a href={slug}>
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">
         {format(availableAt, "EEEE' • 'd' de 'MMMM' • 'K'h'mm", {
           locale: ptBR,
         })}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isPast(availableAt) ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -38,7 +39,7 @@ const Lesson = ({ title, slug, availableAt, type }: LessonProps) => {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 };
 
